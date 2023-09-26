7d1e2c48bad1aa6364c0487475bbb9c4fdf9c0df
Enrickyb <enrickyb@hotmail.com>
2023-09-26 11:44:40 -0400

    :sparkles:feat(prejoin): add a option 'selecione' on select input

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -5,6 +5,11 @@ type SelectType = {
 export default function SelectInput({ options }: SelectType) {
   return (
     <select className="h-10 w-96 rounded text-center">
+      {
+        <option value="" selected disabled>
+          Selecione
+        </option>
+      }
       {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.label}
