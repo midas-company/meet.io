173255bef244dededd285abc5a494ad210e58030
Enrickyb <enrickyb@hotmail.com>
2023-10-16 16:53:36 -0400

    :wrench:fix(prejoin): fix device permissions request

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -6,11 +6,15 @@ type SelectType = {
 export default function SelectInput({ options, ...rest }: SelectType) {
   return (
     <select className="h-10 w-96 rounded text-center" {...rest}>
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
+      {options.length > 0 ? (
+        options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))
+      ) : (
+        <option value="Sem dipositivos">Sem dispositivos</option>
+      )}
     </select>
   );
 }
