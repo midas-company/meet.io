579ad6ae41776e4760633b62f53d971bd16488c2
NatanTavares <nattantavares.s15@gmail.com>
2024-02-18 21:45:40 -0300

    :sparkles:feat(webrtc): implement WebRTC functionality for video call handling. Necessary for establishing real-time communication.

--- src/pages/room/[meetingID]/[peerID]/[userID]/index.tsx
+++ src/pages/room/[meetingID]/[peerID]/[userID]/index.tsx
@@ -2,12 +2,8 @@
 
 import { useRouter } from "next/router";
 import Head from "next/head";
-import axios from "axios";
-import { useEffect, useRef, useState } from "react";
-
-type Sdp = {
-  sdp: string;
-};
+import { useEffect } from "react";
+import { useWebRTC } from "~/hook/useWebRTC";
 
 type Params = {
   meetingID: string;
@@ -19,69 +15,20 @@ export default function Room() {
   const router = useRouter();
   const { meetingID, peerID, userID } = router.query as Params;
 
-  const senderVideo = useRef<HTMLVideoElement>(null);
-  const receiverVideo = useRef<HTMLVideoElement>(null);
+  const {
+    senderVideo,
+    pcSender,
+    setPcSender,
+    senderOniceCandidate,
 
-  const [pcSender, setPcSender] = useState<RTCPeerConnection | null>(null);
-  const [pcReceiver, setPcReceiver] = useState<RTCPeerConnection | null>(null);
+    receiverVideo,
+    pcReceiver,
+    setPcReceiver,
+    receiverOniceCandidate,
 
-  const [isConnected, setIsConnected] = useState(false);
-
-  const handleStartCall = () => {
-    if (!pcReceiver || !pcSender) {
-      console.log("pcReceiver and pcSender is missing!");
-      return;
-    }
-
-    // sender part of the call
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: true })
-      .then((stream) => {
-        if (senderVideo.current) {
-          senderVideo.current.srcObject = stream;
-
-          const tracks = stream.getTracks();
-          tracks.forEach((_, i) => {
-            const mediaTrack = stream.getTracks()[i];
-            if (mediaTrack) {
-              pcSender.addTrack(mediaTrack);
-            }
-          });
-
-          pcSender
-            .createOffer()
-            .then((d) => pcSender.setLocalDescription(d))
-            .catch(console.log);
-        }
-        // you can use event listner so that you inform he is connected!
-        pcSender.addEventListener("connectionstatechange", () => {
-          console.log(pcSender.connectionState);
-          if (pcSender.connectionState === "connected") {
-            console.log("connected!");
-            setIsConnected(true);
-          }
-        });
-
-        // receiver part of the call
-        pcReceiver.addTransceiver("video", { direction: "recvonly" });
-        pcReceiver
-          .createOffer()
-          .then((d) => pcReceiver.setLocalDescription(d))
-          .catch(console.log);
-
-        pcReceiver.ontrack = (event) => {
-          if (receiverVideo.current) {
-            const media = event.streams[0];
-            if (media) {
-              receiverVideo.current.srcObject = media;
-            }
-            receiverVideo.current.autoplay = true;
-            receiverVideo.current.controls = true;
-          }
-        };
-      })
-      .catch(console.log);
-  };
+    isConnected,
+    handleStartCall,
+  } = useWebRTC();
 
   useEffect(() => {
     const newPcSender = new RTCPeerConnection({
@@ -98,57 +45,20 @@ export default function Room() {
       newPcSender.close();
       newPcReceiver.close();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
     if (pcSender) {
-      pcSender.onicecandidate = (event) => {
-        if (event.candidate === null) {
-          axios
-            .post<Sdp>(
-              `https://3578-177-55-229-207.ngrok-free.app/webrtc/sdp/m/${meetingID}/c/${userID}/p/${peerID}/s/true`,
-              {
-                sdp: btoa(JSON.stringify(pcSender.localDescription)),
-              },
-            )
-            .then((res) => {
-              const sdp = JSON.parse(
-                atob(res.data.sdp),
-              ) as RTCSessionDescriptionInit;
-              pcSender
-                .setRemoteDescription(new RTCSessionDescription(sdp))
-                .catch(console.log);
-            })
-            .catch(console.log);
-        }
-      };
+      senderOniceCandidate({ meetingID, peerID, userID });
     }
-  }, [meetingID, pcSender, peerID, userID]);
+  }, [meetingID, pcSender, peerID, senderOniceCandidate, userID]);
 
   useEffect(() => {
     if (pcReceiver) {
-      pcReceiver.onicecandidate = (event) => {
-        if (event.candidate === null) {
-          axios
-            .post<Sdp>(
-              `https://3578-177-55-229-207.ngrok-free.app/webrtc/sdp/m/${meetingID}/c/${userID}/p/${peerID}/s/false`,
-              {
-                sdp: btoa(JSON.stringify(pcReceiver.localDescription)),
-              },
-            )
-            .then((res) => {
-              const sdp = JSON.parse(
-                atob(res.data.sdp),
-              ) as RTCSessionDescriptionInit;
-              pcReceiver
-                .setRemoteDescription(new RTCSessionDescription(sdp))
-                .catch(console.log);
-            })
-            .catch(console.log);
-        }
-      };
+      receiverOniceCandidate({ meetingID, peerID, userID });
     }
-  }, [meetingID, pcReceiver, peerID, userID]);
+  }, [meetingID, pcReceiver, peerID, receiverOniceCandidate, userID]);
 
   return (
     <>
@@ -173,18 +83,17 @@ export default function Room() {
 
         <div className="flex items-center justify-center gap-2">
           <video
-            className="h-[500px] w-[500px]"
+            className="h-[500px] w-[500px] rounded"
+            controls
+            autoPlay
+            ref={receiverVideo}
+          />
+          <video
+            className="h-[160px] w-[160px] rounded"
             controls
             muted
             autoPlay
             ref={senderVideo}
-          />
-          <video
-            className="h-[160px] w-[160px]"
-            controls
-            muted
-            autoPlay
-            ref={receiverVideo}
           />
         </div>
       </main>
